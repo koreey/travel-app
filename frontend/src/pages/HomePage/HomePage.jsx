cd3f057356c4b14e4a88389e706fc7eff2e16dd2
KoreyOcampo <koreyocampo@gmail.com>
2025-01-30 19:16:17 -0800

    updated title

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
 export default function HomePage() {
   
-  return <h1 style={{ fontWeight: 'bold', color: '#4169E1', fontSize: '3rem'  }}>Where To Next?</h1>;
+  return <h1 style={{ fontWeight: 'bold', color: '#4169E1', fontSize: '4rem'  }}>Where To Next?</h1>;
 }
